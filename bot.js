14be922c643290cdfe08c69e3b5017c20f838a60
ahmadomaxherenow <41526951+JFHWYFHEDY9FR7923RYBHFKDSCF@users.noreply.github.com>
2019-06-12 19:05:28 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+ 
+ 
+ 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Model Codes`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Model Codes ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(argresult, "https://www.twitch.tv/dggamingbot");//ÕﬁÊﬁ „ÊœÌ· ﬂÊœ“
+client.user.setStatus("online")
+ 
+});
